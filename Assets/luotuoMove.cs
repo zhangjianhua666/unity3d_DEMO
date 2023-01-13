e794a0a554fa56b7d5c2dd9222eb426f76c0ad13
ZhangJianHua <2207896513@QQ.com>
2023-01-13 20:40:50 +0800

    ADD项目文件

---
+++ Assets/luotuoMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class luotuoMove : MonoBehaviour
+{
+    public Text txt1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string a = "asdd";
+        txt1.text = a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
